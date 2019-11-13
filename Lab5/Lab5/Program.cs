6abe9210bfa4953e6753645977b99363d3f7f444
tlyon1022 <54088092+tlyon1022@users.noreply.github.com>
2019-11-13 12:32:54 -0500

    Add files via upload

---
+++ Lab5/Lab5/Program.cs
@@ -0,0 +1,59 @@
+﻿/*
+ * K9195
+ * CIS 199-02
+ * Lab 5
+ * Oct 20, 2019
+ * A console app that utilizes For loops in order to create 4 different patterns
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+namespace Lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int MAX_ROWS = 10;//Constant to set the max amount of rows each outer For loop will create
+            WriteLine("Pattern A");
+            for (int row = 1; row <= MAX_ROWS; row++)
+            {
+                for (int star = 1; star <= row; star++)
+                Write("*");
+                WriteLine();
+            }
+
+            WriteLine("Pattern B");
+            for (int row = MAX_ROWS; row >= 1; row--)
+            {
+                for (int star = 1; star <= row; star++)
+                    Write("*");
+                WriteLine();
+            }
+            WriteLine("Pattern C");
+            
+            for (int row = MAX_ROWS; row >= 1; row--)
+            {
+                for (int space = 1; space <= MAX_ROWS-row; space++)
+                    Write(" ");
+                for (int star=1; star <=row; star++)
+                    Write("*");
+                WriteLine();
+            }
+        
+            WriteLine("Pattern D");
+            for (int row = 1; row <= MAX_ROWS; row++)
+            {
+                for (int space = 1; space <= MAX_ROWS - row; space++)
+                    Write(" ");
+                for (int star = 1; star <= row; star++)
+                    Write("*");
+                WriteLine();
+            }
+
+        }
+    }
+}
